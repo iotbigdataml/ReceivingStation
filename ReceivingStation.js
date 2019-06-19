655e2290641ff527e6cc774fa03fafd0de22d20f
Krishna Guru <38326447+Krishna-Guru@users.noreply.github.com>
2019-06-19 02:15:18 -0400

    Add files via upload

---
+++ ReceivingStation.js
@@ -0,0 +1,115 @@
+var app=angular.module("receivingstation",[]);
+app.controller("receivingcontroller",['$scope','$http', function($scope, $http){
+
+// Fetch data from url every one second
+
+ setInterval(function(){
+  $http({
+      method: 'GET',
+      url: 'http://127.0.0.1:8080/test.txt'
+
+    }).then(function successCallback(response) {
+
+      $scope.users = response.data;
+
+    }, function errorCallback(response) {
+
+      alert("Error. Try Again!");
+
+    })
+},1000);
+
+// Function to control bot one
+        $scope.botone = function(){
+          $http({
+              method: 'GET',
+              url: 'http://29041a89.ngrok.io/bot1'
+
+            }).then(function successCallback(response) {
+
+
+
+            }, function errorCallback(response) {
+
+              alert("Error. Try Again!");
+
+            });
+
+
+        };
+
+// Function to control bot two
+
+        $scope.bottwo = function(){
+          $http({
+              method: 'GET',
+              url: 'http://29041a89.ngrok.io/bot2'
+
+            }).then(function successCallback(response) {
+
+
+
+
+            }, function errorCallback(response) {
+
+              alert("Error. Try Again!");
+
+            });
+
+
+        };
+
+// Function to put bots on maintenance
+
+        $scope.maintainence = function(){
+          $http({
+              method: 'GET',
+              url: 'http://29041a89.ngrok.io/maintenance'
+
+            }).then(function successCallback(response) {
+
+
+
+
+            }, function errorCallback(response) {
+
+              alert("Error. Try Again!");
+
+            });
+
+
+        };
+
+
+
+
+}]);
+
+// filter to group orders by ID
+
+app.filter("groupBy",["$parse","$filter",function($parse,$filter){
+  return function(array,groupByField){
+    var result	= [];
+            var prev_item = null;
+            var groupKey = false;
+            var filteredData = $filter('orderBy')(array,groupByField);
+            for(var i=0;i<filteredData.length;i++){
+              groupKey = false;
+              if(prev_item !== null){
+                if(prev_item[groupByField] !== filteredData[i][groupByField]){
+                  groupKey = true;
+                }
+              } else {
+                groupKey = true;
+              }
+              if(groupKey){
+                filteredData[i]['group_by_key'] =true;
+              } else {
+                filteredData[i]['group_by_key'] =false;
+              }
+              result.push(filteredData[i]);
+              prev_item = filteredData[i];
+            }
+            return result;
+  }
+}]);
